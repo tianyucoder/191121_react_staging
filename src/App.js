793ac7ed522ab33b6d929c5a007d8433d23ecab2
tianyu5177 <zhangtianyu5177@sina.com>
2020-04-25 12:04:49 +0800

    更新

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React, { Component } from 'react'
-import Search from './components/search'
-import List from './components/list'
+import About from './components/about/about'
+import Home from './components/home/home'
 
 export default class App extends Component {
 	render() {
 		return (
-			<div id="app">
-				<div className="container">
-					<Search/>
-					<List/>
+			<div>
+				<div className="row">
+					<div className="col-xs-offset-2 col-xs-8">
+						<div className="page-header"><h2>React Router Demo</h2></div>
+					</div>
+				</div>
+				<div className="row">
+					<div className="col-xs-2 col-xs-offset-2">
+						<div className="list-group">
+							<a className="list-group-item " href="./about.html">About</a>
+							<a className="list-group-item" href="./home.html">Home</a>
+						</div>
+					</div>
+					<div className="col-xs-6">
+						<div className="panel">
+							<div className="panel-body">
+								<About/>
+								<Home/>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		)
